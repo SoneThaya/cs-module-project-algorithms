d832ee3fe6ffc12b854ca5e5292c57f24fb07478
Sone <sthaya@gmail.com>
2020-07-23 15:12:47 -0700

    complted most

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,26 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+def eating_cookies(n, cache={0:1, 1:1, 2:2}):
     # Your code here
+   
+    # if n < 0:
+    #     return 0
+    # if n == 0:
+    #     return 1
+    # elif n in cache:
+    #     return cache[n]
+    # else:
+        
+    #     cache[n] = eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    #     return cache[n]
+    if n in cache:
+        return cache[n]
+    else:
+        cache[n] = eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+        return cache[n]
+    
 
-    pass
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -4,8 +4,24 @@ import sys
 
 def rock_paper_scissors(n):
   # Your code here
-
-  pass
+  options = ['rock', 'paper', 'scissors']
+  all_possibilities = []
+  
+  if n == 0:
+    return [[]]
+  if n == 1:
+    return options
+  
+  def round_choice(round, round_number):
+    
+    for i in options:
+      round.append(options[i])
+      if round_number == n:
+        all_possibilities.append(round.slice())
+      else:
+        round_choice(round, round_number +1)
+  
+  return all_possibilities
 
 
 if __name__ == "__main__":
